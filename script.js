2e4114b9e705360b6aa787d47083179b71fc43d7
carruselices <carruselices@gmail.com>
2025-07-28 23:41:33 -0300

    Web Carrusel ICES

---
+++ script.js
@@ -0,0 +1,22 @@
+const images = [
+  "images/foto1.jpg",
+  "images/foto2.jpg",
+  "images/foto3.jpg"
+];
+
+let current = 0;
+const slideEl = document.getElementById("slide");
+
+function changeSlide() {
+  slideEl.classList.remove("active");
+  setTimeout(() => {
+    slideEl.src = images[current];
+    slideEl.onload = () => slideEl.classList.add("active");
+    current = (current + 1) % images.length;
+  }, 500);
+}
+
+// Inicial
+slideEl.src = images[0];
+slideEl.onload = () => slideEl.classList.add("active");
+setInterval(changeSlide, 5000);
