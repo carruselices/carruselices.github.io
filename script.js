76fcc3527f5d3b3ce1013d33e7a82b169df45c07
carruselices <carruselices@gmail.com>
2025-07-30 17:11:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1 }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 6000);
+  setTimeout(showSlides, 7000);
 }
